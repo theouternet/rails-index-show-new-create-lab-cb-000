ebb75393d1fcb614a1fda1f0ae0411063c970d22
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-25 01:43:22 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_coupons.rb
+++ db/migrate/001_create_coupons.rb
@@ -1,10 +1,10 @@
 class CreateCoupons < ActiveRecord::Migration
-  def create 
+  def change
     create_table :coupons do |t|
       t.string :coupon_code
       t.string :store
-      
-            t.timestamps null: false
-end 
-end 
-end 
+
+      t.timestamps null: false
+    end
+  end
+end
